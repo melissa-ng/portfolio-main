ce2d9522732e48ad1228fde00bc120713fd7a9a9
melissa-ng <melissa.j.ng-1@ou.edu>
2024-06-03 18:26:02 -0500

    initial pages

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 function HomePage() {
   return <div>
     <h1>Welcome to my portfolio! This is a single page application!
-      I built this using Next.js and deployed it on netlify. I also used MUI components like the floating action buttons on the bottom right hand corner and icons from Font Awesome.
+      I built this using Next.js and deployed it on Vercel. I also used MUI components like the floating action buttons on the bottom right hand corner and icons from Font Awesome.
     </h1>
   </div>
 }
